92777879bf54fe58e50a20c2598b1c5882850837
Ciel Bellerose <kcbellerose@icloud.com>
2025-11-02 11:38:09 -0500

    basic button + modal

--- frontend/src/components/pages/AddPostModal.jsx
+++ frontend/src/components/pages/AddPostModal.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
-import ReactModal from "react-modal";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
+// https://react-bootstrap.netlify.app/docs/components/modal/
+// Vertically centered modal
 export default function AddPostModal() {
   console.log("Hello from React!");
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
@@ -12,11 +14,20 @@ export default function AddPostModal() {
       <Button variant="primary" onClick={handleShow}>
         Add Post
       </Button>
-      <ReactModal show={show} onHide={handleClose}>
-        <h2>Add a New Post</h2>
-      </ReactModal>
-
-      <h1>Index Page</h1>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
