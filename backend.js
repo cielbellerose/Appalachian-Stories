2b67ccbc9bc9b97081946813c7d99e29ece98484
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-02 13:56:54 -0700

    Created route && geographic testdata

--- backend.js
+++ backend.js
@@ -1,4 +1,6 @@
 import express from "express";
+import staticTestCoodinates from "./db/testData.json" with {type:"json"}; //this should be removed after db migration
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,3 +13,11 @@ app.use("/", express.static("./frontend/dist"));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//this route just returns static files test JSON data to not have to deal with
+//seting up data flows quite yet
+app.use("/api/test",(req,res) => {
+  console.log("Sending JSON test data",staticTestCoodinates);
+  res.json(staticTestCoodinates);
+});
+
