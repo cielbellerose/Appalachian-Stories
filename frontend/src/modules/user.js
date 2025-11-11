20ea65e401378f40c0d3d63639cd885ac305a8ba
Ciel Bellerose <kcbellerose@icloud.com>
2025-11-11 15:18:10 -0500

    current user

--- frontend/src/modules/user.js
+++ frontend/src/modules/user.js
@@ -2,11 +2,14 @@ const user = {};
 
 user.getCurrentUser = async () => {
   try {
+    console.log("entered try of get current user...")
     const response = await fetch("/api/current_user", {
       credentials: "include",
     });
     if (response.ok) {
+      console.log("response was okayy!!! response", response)
       const data = await response.json();
+      console.log("response.json : ", data)
       return data.username;
     }
     return null;
